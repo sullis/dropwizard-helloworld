3457a3a08ea9e3d3e39461621076882d8d1a54a4
Sean C. Sullivan <github@seansullivan.com>
2020-07-14 20:03:43 -0700

    cleanup whitespace

--- src/main/java/com/github/sullis/dropwizard/helloworld/HelloWorldApplication.java
+++ src/main/java/com/github/sullis/dropwizard/helloworld/HelloWorldApplication.java
@@ -4,23 +4,24 @@ import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
-public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
+public class HelloWorldApplication
+    extends Application<HelloWorldConfiguration> {
 
     public static void main(String[] args) throws Exception {
         new HelloWorldApplication().run(args);
     }
 
     @Override
-	public String getName() { return "helloworld"; }
+    public String getName() { return "helloworld"; }
 
-	@Override
-	public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
-		// nothing to do yet
-	}
+    @Override
+    public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
+        // nothing to do yet
+    }
 
-	@Override
-	public void run(HelloWorldConfiguration configuration,
-					Environment environment) {
-		// nothing to do yet
-	}
+    @Override
+    public void run(HelloWorldConfiguration configuration,
+                    Environment environment) {
+        // nothing to do yet
+    }
 }
