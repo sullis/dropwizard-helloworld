3448897598e9d5507222b9cb510eeed669286c08
Sean C. Sullivan <github@seansullivan.com>
2020-07-14 21:04:51 -0700

    add 'defaultName' to HelloWorldConfiguration

--- src/main/java/com/github/sullis/dropwizard/helloworld/HelloWorldConfiguration.java
+++ src/main/java/com/github/sullis/dropwizard/helloworld/HelloWorldConfiguration.java
@@ -1,7 +1,20 @@
 package com.github.sullis.dropwizard.helloworld;
 
 import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import javax.validation.constraints.NotEmpty;
 
 public class HelloWorldConfiguration extends Configuration {
+    @NotEmpty
+    private String defaultName;
 
+    @JsonProperty
+    public String getDefaultName() {
+        return defaultName;
+    }
+
+    @JsonProperty
+    public void setDefaultName(String name) {
+        this.defaultName = name;
+    }
 }
